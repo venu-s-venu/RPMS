9c9d9bb16e5771fe15810bb1c059b4bb986031df
CodeWithVenu <venusvenu91456@gmail.com>
2026-02-03 22:17:39 +0530

    Update view-pass-detail.php

--- view-pass-detail.php
+++ view-pass-detail.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
 if(strlen($_SESSION['bpmsaid']==0)){
-  header('location:logout.php'); 
+  header('location:logout.php');  
   } else{
   ?> 
 <!DOCTYPE html>
@@ -135,4 +135,5 @@ foreach($results as $row)
 </html>
 
 <?php }  ?>
+
 
